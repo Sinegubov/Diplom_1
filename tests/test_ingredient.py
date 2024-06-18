c6c7397b1c67e5b3d34dc1ff51fcc35ee49a519f
Sinegubov Pavel <p.sinegubov@bi.zone>
2024-06-19 00:40:48 +0700

    Add test bun and test ingredient

--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -3,17 +3,17 @@ from ingredient import Ingredient
 
 
 class TestIngredient:
-    @pytest.mark.parametrize("ingredient_type", ["Острый соус", "gorchitsa", ""])
+    @pytest.mark.parametrize("ingredient_type", ["SAUCE", "FILLING"])
     def test_ingredient_type(self, ingredient_type):
         type_ingr = Ingredient(ingredient_type, name='def_name_ingr', price=1).type
         assert type_ingr == ingredient_type
 
     @pytest.mark.parametrize("name", ["1000 Островов", "Tar-Tar", ""])
     def test_ingredient_name(self, name):
-        name_ingr = Ingredient(ingredient_type="def_type_name", name=name, price=1).name
+        name_ingr = Ingredient(ingredient_type="SAUCE", name=name, price=1).name
         assert name_ingr == name
 
     @pytest.mark.parametrize("price", [0, 666666, 1.23, -2])
     def test_ingredient_price(self, price):
-        price_ingr = Ingredient(ingredient_type="def_type_name", name='def_name_ingr', price=price).price
+        price_ingr = Ingredient(ingredient_type="FILLING", name='def_name_ingr', price=price).price
         assert price_ingr == price
